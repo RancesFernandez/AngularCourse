e3a36149e57fa650657309384bde7037e5bd6aef
Rances <carlosrancesfb1991@gmail.com>
2021-03-09 15:23:34 -0300

    heror-tarjeta-component done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { AboutComponent } from './components/about/about.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { SeekerComponent } from './components/seeker/seeker.component';
 
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'heroes', component: HeroesComponent },
+  { path: 'heroe/:id', component: HeroeComponent},
+  { path: 'search/:term', component: SeekerComponent},
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
---
+++ src/app/components/seeker/seeker.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-seeker',
+  templateUrl: './seeker.component.html',
+ 
+})
+export class SeekerComponent implements OnInit {
+
+  heroes: any[] = [];
+  term: string;
+
+  constructor( private activatedRoute: ActivatedRoute,
+               private _heroesService: HeroesService ) { }
+
+  ngOnInit(){
+
+    this.activatedRoute.params.subscribe( params => {
+      this.term = (params['term']);
+      this.heroes = this._heroesService.searchHeroes(params['term']);
+      console.log(this.heroes);
+    } )
+
+  }
+
+}
