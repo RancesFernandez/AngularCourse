87d52311d6d3fb6213eeb63d1fb899df1decd7f7
Carlos Fernández <carlosrancesfb1991@gmail.com>
2021-04-05 17:06:39 -0300

    service and list creation

---
+++ src/app/models/list-item.model.ts
@@ -0,0 +1,13 @@
+
+
+export class ListItem{
+
+    desc: string;
+    finished: boolean;
+
+    constructor(desc: string){
+        this.desc = desc;
+        this.finished = false;
+    }
+
+} 
---
+++ src/app/services/wishes.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { List } from '../models/list.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WishesService {
+
+  lists: List[] = [];
+
+  constructor() { 
+
+    const list1 = new List( 'Recolectar piedras del infinito' );
+    const list2 = new List( 'Heroes a desaparecer' );
+
+    this.lists.push(list1, list2);
+
+  
+  }
+}
